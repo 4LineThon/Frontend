76bcb7614c89e31b65401ca34c8c6db4971dfad3
Youbin Jang <120731400+youbing5921@users.noreply.github.com>
2024-11-08 09:07:14 +0900

    [Refactor] 코멘트 CSS 정리

--- src/comment/Comment.js
+++ src/comment/Comment.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
+import CommentBox from "./component/commentBox";
+import CommentInput from "./component/commentInput";
+import { Wrapper } from "./component/commentCSS";
 import styled from "styled-components";
-import { Bullet, Commemt, Content, Wrapper, Writer } from "./component/comment";
 
 const Comment = () => {
   const [comments, setComments] = useState([]);
@@ -20,61 +22,15 @@ const Comment = () => {
   }, []);
 
   return (
-    <>
-      {comments.map((elt, idx) => {
-        return (
-          <CommentWrapper key={idx}>
-            <Bullet />
-            <Commemt>
-              <Writer>Comment by {elt.name}</Writer>
-              <Content>{elt.content}</Content>
-            </Commemt>
-          </CommentWrapper>
-        );
+    <Wrapper>
+      {comments.map((comment, idx) => {
+        return <CommentBox key={idx} commentInfo={comment} />;
       })}
-      <InputWrapper>
-        <Bullet />
-        <TextArea />
-        <SendButton />
-      </InputWrapper>
-    </>
+      <CommentInput />
+    </Wrapper>
   );
 };
 
 export default Comment;
 
-const CommentWrapper = styled(Wrapper)`
-  border-color: #9ea663;
-`;
-
-const InputWrapper = styled(Wrapper)`
-  border-color: #423e59;
-`;
-
-const TextArea = styled.textarea.attrs({
-  placeholder: "Write yout comment",
-  maxLength: 25,
-})`
-  width: 158px;
-  height: 25px;
-  border: 0;
-  outline: 0;
-  padding: 0;
-  background-color: transparent;
-  font-size: 10px;
-  color: #423e59;
-  font-family: "Ibarra Real Nova", serif;
-  resize: none;
-  &::placeholder {
-    color: #423e59;
-    font-family: "Ibarra Real Nova", serif;
-  }
-`;
-
-const SendButton = styled.img.attrs({
-  src: "/send-comment.svg",
-})`
-  width: 20px;
-  height: 20px;
-  margin: auto 0 auto 8px;
-`;
+const Wrapper = styled.div``;
