9f354a4e883d71b5f2be5a99a39f432a96013a01
Youbin Jang <120731400+youbing5921@users.noreply.github.com>
2024-11-15 00:37:09 +0900

    Update commentCSS.jsx

--- src/comment/component/commentCSS.jsx
+++ src/comment/component/commentCSS.jsx
@@ -4,13 +4,12 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   width: 211px;
   display: flex;
-  align-items: center;
   margin: auto; /* Center alignment */
   padding: 9px;
-  border: 2px solid;
+  border-width: 3px;
+  border-style: solid;
   font-family: "Ibarra Real Nova", serif;
   color: #423e59;
-  text-align: center; /* Center alignment for text */
 `;
 
 export const Bullet = styled.img.attrs({
@@ -25,7 +24,6 @@ export const Commemt = styled.div`
   display: flex;
   flex-direction: column;
   gap: 7px;
-  text-align: center; /* Center alignment for text inside comment */
 `;
 
 export const Writer = styled.div`
@@ -34,5 +32,4 @@ export const Writer = styled.div`
 
 export const Content = styled.div`
   font-size: 10px;
-  text-align: center;
 `;
